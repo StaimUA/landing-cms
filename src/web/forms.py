559cc4f9733ffb37568741765d516f8744c1c8be
StaimUA <aroyo@ukr.net>
2019-06-06 23:29:46 +0300

    Add some placeholders to contact form

--- src/web/forms.py
+++ src/web/forms.py
@@ -3,6 +3,12 @@ from django.forms import ModelForm
 from .models import ContactForm
 
 class ContactForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].widget.attrs['placeholder'] = 'Your E-mail'
+        self.fields['full_name'].widget.attrs['placeholder'] = 'Your Full Name'
+        self.fields['comment'].widget.attrs['placeholder'] = 'Your comments'
+
     class Meta:
         model = ContactForm
         fields = ['email', 'full_name', 'comment']
